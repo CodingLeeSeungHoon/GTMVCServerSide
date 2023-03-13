a9f19e3b00a6c418dd16860feaa36c4a19f316ba
CodingLeeSeungHoon <free_minkya@naver.com>
2023-03-13 16:37:06 +0900

    Initial commit

---
+++ src/main/java/com/example/gtmvcserverside/GtmvcServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.example.gtmvcserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmvcServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtmvcServerSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtmvcserverside/GtmvcServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtmvcserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmvcServerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
